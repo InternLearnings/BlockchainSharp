7dc8ea6053ed330d3eec4f1fbb884b594313b2b7
ajlopez <webmaster@ajlopez.com>
2018-04-16 13:36:55 -0300

    TransactionPool with empty list

---
+++ Src/BlockchainSharp.Tests/Stores/TransactionPoolTests.cs
@@ -0,0 +1,18 @@
+﻿namespace BlockchainSharp.Tests.Stores
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Stores;
+
+    [TestClass]
+    public class TransactionPoolTests
+    {
+        [TestMethod]
+        public void CreateWithEmptyList()
+        {
+            TransactionPool pool = new TransactionPool();
+
+            Assert.AreEqual(0, pool.Transactions.Count);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Stores/TransactionPool.cs
@@ -0,0 +1,15 @@
+﻿namespace BlockchainSharp.Stores
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TransactionPool
+    {
+        private IList<Transaction> transactions = new List<Transaction>();
+
+        public IList<Transaction> Transactions { get { return this.transactions; } }
+    }
+}
