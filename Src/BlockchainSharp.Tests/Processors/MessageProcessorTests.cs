e599b4584ec1ca627f86f7335a3f4171f8ebaf2f
ajlopez <webmaster@ajlopez.com>
2018-05-06 13:27:37 -0300

    MessageProcessor process BlockMessage

---
+++ Src/BlockchainSharp.Tests/Processors/MessageProcessorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace BlockchainSharp.Tests.Processors
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Tests.TestUtils;
+    using BlockchainSharp.Processors;
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Net.Messages;
+
+    [TestClass]
+    public class MessageProcessorTests
+    {
+        [TestMethod]
+        public void ProcessBlockMessage()
+        {
+            BlockProcessor blockProcessor = FactoryHelper.CreateBlockProcessor();
+            Block block = FactoryHelper.CreateGenesisBlock();
+            BlockMessage message = new BlockMessage(block);
+
+            MessageProcessor processor = new MessageProcessor(blockProcessor);
+
+            processor.ProcessMessage(message);
+
+            Assert.IsNotNull(blockProcessor.BlockChain);
+            Assert.AreEqual(0, blockProcessor.BlockChain.BestBlockNumber);
+            Assert.AreEqual(block, blockProcessor.BlockChain.GetBlock(0));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Processors/MessageProcessor.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Processors
+{
+    using BlockchainSharp.Net.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MessageProcessor
+    {
+        private BlockProcessor blockProcessor;
+
+        public MessageProcessor(BlockProcessor blockProcessor)
+        {
+            this.blockProcessor = blockProcessor;
+        }
+
+        public void ProcessMessage(Message message)
+        {
+            this.blockProcessor.Process(((BlockMessage)message).Block);
+        }
+    }
+}
