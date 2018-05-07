a16f0242980d010dda1f21ddf647fb06a634f90b
ajlopez <webmaster@ajlopez.com>
2018-05-07 12:18:26 -0300

    Mine block, first implementation

---
+++ Src/BlockchainSharp.Tests/Processors/MinerProcessorTests.cs
@@ -0,0 +1,28 @@
+﻿namespace BlockchainSharp.Tests.Processors
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Stores;
+    using BlockchainSharp.Processors;
+    using BlockchainSharp.Tests.TestUtils;
+    using BlockchainSharp.Core;
+
+    [TestClass]
+    public class MinerProcessorTests
+    {
+        [TestMethod]
+        public void MineBlock()
+        {
+            TransactionPool transactionPool = new TransactionPool();
+            MinerProcessor processor = new MinerProcessor(transactionPool);
+            Block genesis = FactoryHelper.CreateGenesisBlock();
+
+            Block block = processor.MineBlock(genesis);
+
+            Assert.IsNotNull(block);
+            Assert.AreEqual(1, block.Number);
+            Assert.AreEqual(0, block.Transactions.Count);
+            Assert.AreEqual(genesis.Hash, block.ParentHash);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Processors/MinerProcessor.cs
@@ -0,0 +1,24 @@
+﻿namespace BlockchainSharp.Processors
+{
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Stores;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MinerProcessor
+    {
+        private TransactionPool transactionPool;
+
+        public MinerProcessor(TransactionPool transactionPool)
+        {
+            this.transactionPool = transactionPool;
+        }
+
+        public Block MineBlock(Block parent)
+        {
+            return new Block(parent.Number + 1, parent.Hash);
+        }
+    }
+}
