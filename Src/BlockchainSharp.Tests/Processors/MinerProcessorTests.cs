7fc255ea23f43bb15fcc3db590a45213b7dcdb02
ajlopez <webmaster@ajlopez.com>
2018-05-09 17:13:49 -0300

    Additional miner process test

--- Src/BlockchainSharp.Tests/Processors/MinerProcessorTests.cs
+++ Src/BlockchainSharp.Tests/Processors/MinerProcessorTests.cs
@@ -43,5 +43,26 @@
             Assert.AreSame(transaction, block.Transactions[0]);
             Assert.AreEqual(genesis.Hash, block.ParentHash);
         }
+
+        [TestMethod]
+        public void MineBlockWithTwoTransactions()
+        {
+            TransactionPool transactionPool = new TransactionPool();
+            Transaction transaction1 = FactoryHelper.CreateTransaction(1000);
+            Transaction transaction2 = FactoryHelper.CreateTransaction(2000);
+            transactionPool.AddTransaction(transaction1);
+            transactionPool.AddTransaction(transaction2);
+
+            MinerProcessor processor = new MinerProcessor(transactionPool);
+            Block genesis = FactoryHelper.CreateGenesisBlock();
+
+            Block block = processor.MineBlock(genesis);
+
+            Assert.IsNotNull(block);
+            Assert.AreEqual(1, block.Number);
+            Assert.AreEqual(2, block.Transactions.Count);
+            Assert.IsTrue(block.Transactions.Contains(transaction1));
+            Assert.IsTrue(block.Transactions.Contains(transaction2));
+        }
     }
 }
