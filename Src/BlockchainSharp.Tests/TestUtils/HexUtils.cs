cda8bcab4ffd90a72b77200ab328f1efbf0f88e7
ajlopez <webmaster@ajlopez.com>
2018-05-26 11:42:29 -0300

    Adding string to byte array utility

---
+++ Src/BlockchainSharp.Tests/TestUtils/HexUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Tests.TestUtils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HexUtils
+    {
+        public static byte[] StringToByteArray(string hex)
+        {
+            int nchars = hex.Length;
+            byte[] bytes = new byte[nchars / 2];
+
+            for (int k = 0; k < nchars; k += 2)
+                bytes[k / 2] = Convert.ToByte(hex.Substring(k, 2), 16);
+
+            return bytes;
+        }
+    }
+}
---
+++ Src/BlockchainSharp.Tests/TestUtils/HexUtilsTests.cs
@@ -0,0 +1,71 @@
+﻿namespace BlockchainSharp.Tests.TestUtils
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HexUtilsTests
+    {
+        [TestMethod]
+        public void ConvertEmptyStringToZeroLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray(string.Empty);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void ConvertStringTOneLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray("0f");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(15, result[0]);
+        }
+
+        [TestMethod]
+        public void ConvertStringToOneLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray("0f");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(0x0f, result[0]);
+        }
+
+        [TestMethod]
+        public void ConvertStringToTwoLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray("cafe");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0xca, result[0]);
+            Assert.AreEqual(0xfe, result[1]);
+        }
+
+        [TestMethod]
+        public void ConvertUpperCaseStringToTwoLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray("CAFE");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0xca, result[0]);
+            Assert.AreEqual(0xfe, result[1]);
+        }
+
+        [TestMethod]
+        public void ConvertMixedCaseStringToTwoLengthByteArray()
+        {
+            var result = HexUtils.StringToByteArray("CaFe");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0xca, result[0]);
+            Assert.AreEqual(0xfe, result[1]);
+        }
+    }
+}
