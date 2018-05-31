638a2df2507e241318bf1ea7933e50b572607592
ajlopez <webmaster@ajlopez.com>
2018-05-31 19:01:54 -0300

    BlockHash core type

--- Src/BlockchainSharp/Core/BlockHeader.cs
+++ Src/BlockchainSharp/Core/BlockHeader.cs
@@ -14,7 +14,7 @@
         private Hash parentHash;
         private Hash transactionsHash;
 
-        private Hash hash;
+        private BlockHash hash;
 
         public BlockHeader(long number, Hash parentHash, Hash transactionsHash)
         {
@@ -29,28 +29,17 @@
 
         public Hash TransactionsHash { get { return this.transactionsHash; } }
 
-        public Hash Hash
+        public BlockHash Hash
         {
             get
             {
                 if (this.hash != null)
                     return this.hash;
 
-                this.hash = this.CalculateHash();
+                this.hash = new BlockHash(this);
 
                 return this.hash;
             }
-        }
-
-        private Hash CalculateHash()
-        {
-            Sha3Digest digest = new Sha3Digest(256);
-            byte[] bytes = BlockHeaderEncoder.Instance.Encode(this);
-            digest.BlockUpdate(bytes, 0, bytes.Length);
-            byte[] result = new byte[32];
-            digest.DoFinal(result, 0);
-
-            return new Hash(result);
         }
     }
 }
---
+++ Src/BlockchainSharp/Core/Types/BlockHash.cs
@@ -0,0 +1,28 @@
+﻿namespace BlockchainSharp.Core.Types
+{
+    using BlockchainSharp.Encoding;
+    using Org.BouncyCastle.Crypto.Digests;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockHash : Hash
+    {
+        public BlockHash(BlockHeader header)
+            : base(CalculateHash(header))
+        {
+        }
+
+        private static byte[] CalculateHash(BlockHeader header)
+        {
+            Sha3Digest digest = new Sha3Digest(256);
+            byte[] bytes = BlockHeaderEncoder.Instance.Encode(header);
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[32];
+            digest.DoFinal(result, 0);
+
+            return result;
+        }
+    }
+}
