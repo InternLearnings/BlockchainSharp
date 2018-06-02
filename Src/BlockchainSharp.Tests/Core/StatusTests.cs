4b415ee1ce0392d95de68147f1db4c6e89ab269c
ajlopez <webmaster@ajlopez.com>
2018-06-02 12:21:07 -0300

    Status, first simple test

---
+++ Src/BlockchainSharp.Tests/Core/StatusTests.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Core.Types;
+    using BlockchainSharp.Core;
+
+    [TestClass]
+    public class StatusTests
+    {
+        [TestMethod]
+        public void CreateStatus()
+        {
+            ulong height = 42;
+            BlockHash hash = new BlockHash(new byte[] { 1, 2, 4 });
+
+            Status status = new Status(height, hash);
+
+            Assert.AreEqual(height, status.Height);
+            Assert.AreEqual(hash, status.Hash);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/Status.cs
@@ -0,0 +1,24 @@
+﻿namespace BlockchainSharp.Core
+{
+    using BlockchainSharp.Core.Types;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Status
+    {
+        private ulong height;
+        private BlockHash hash;
+
+        public Status(ulong height, BlockHash hash)
+        {
+            this.height = height;
+            this.hash = hash;
+        }
+
+        public ulong Height { get { return this.height; } }
+
+        public BlockHash Hash { get { return this.hash; } }
+    }
+}
